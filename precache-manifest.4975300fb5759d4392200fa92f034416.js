0f327a5366918145ebcdcd0d939e20a325c1e043
Ashutosh Kumar <ashuyadav2554@gmail.com>
2022-10-14 18:11:55 +0530

    Updates

---
+++ precache-manifest.4975300fb5759d4392200fa92f034416.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f3ad6659df753533fd91b68140b302",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/index.html"
+  },
+  {
+    "revision": "fd46d78c29fe1c800703",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/css/2.836a6e9d.chunk.css"
+  },
+  {
+    "revision": "42fe59e08d1f9c97edf2",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/css/main.ff7b665d.chunk.css"
+  },
+  {
+    "revision": "fd46d78c29fe1c800703",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/js/2.942b19de.chunk.js"
+  },
+  {
+    "revision": "914a85462fc888825cb3a498c2b9869c",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/js/2.942b19de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42fe59e08d1f9c97edf2",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/js/main.3a62ecca.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/js/main.3a62ecca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c716a5b6ee173310b578",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/js/runtime-main.39975228.js"
+  },
+  {
+    "revision": "1907b12c0839c20f294efc404f1c1d60",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/media/Assamjpg.1907b12c.jpg"
+  },
+  {
+    "revision": "31e1300d419245fd27614630601dc74d",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/media/bootstrap-icons.31e1300d.woff2"
+  },
+  {
+    "revision": "be6df3f0c9855796ae0c099ee4ca43ce",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/media/bootstrap-icons.be6df3f0.woff"
+  },
+  {
+    "revision": "c6d48d556870798219984b891e01dee2",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/media/delhi.c6d48d55.jpeg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "475cd43809eed4f7842a6bcf31bc56fa",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/media/jandk.475cd438.jpg"
+  },
+  {
+    "revision": "37b42bbc2b5c1d86da4203690ada78a7",
+    "url": "//ashutoshky.github.io/MyIndia-Crud_React/static/media/rajasthan.37b42bbc.jpg"
+  }
+]);
